37263db5c2bee811b61ef0813344656d17d267e1
apaulaleao <paula.leao@gmail.com>
2021-10-21 22:55:22 -0300

    correcao api

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 const Api = {
- apiUrl: "http://localhost:3001/lista",
+ apiUrl: "https://backend-todoana.herokuapp.com/lista",
   fetchGetAll: () => fetch(Api.apiUrl),
   fetchGetById: id => fetch(`${Api.apiUrl}/${id}`),
   fetchPost: (lista) => {
